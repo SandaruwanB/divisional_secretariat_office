f6d38d87fbbce099350cbbce317a48a6be1b4a68
SandaruwanB <sandarusbandara110@gmail.com>
2024-05-24 11:00:11 +0530

    [DEV] responsiveness

--- resources/views/layouts/homenavigation.blade.php
+++ resources/views/layouts/homenavigation.blade.php
@@ -15,13 +15,13 @@
 
     <body class="font-sans antialiased">
         <nav class="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200">
-            <div class=" max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-5">
+            <div class=" max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-2 py-7">
                 <a href="{{ route('home') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
                     <span class="self-center md:text-2xl text-sm font-semibold whitespace-nowrap text-gray-600">DIVISIONAL SECRETARIAT <br/> KOTAPOLA</span>
                 </a>
                 <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                     @if (Route::has('login'))
-                        <a href="{{ route('login') }}" class="text-white bg-green-600 hover:bg-green-800 focus:ring-2 focus:outline-none focus:ring-green-300 font-semibold rounded-lg text-sm md:px-5 md:py-3 px-3 py-2 text-center">
+                        <a href="{{ route('login') }}" class="text-white bg-green-500 hover:bg-green-600 focus:ring-2 focus:outline-none focus:ring-green-300 font-semibold rounded-xl text-md md:px-5 md:py-3 px-3 py-2 text-center">
                             Login
                         </a>
                     @endif
@@ -30,6 +30,7 @@
                     </button>
                 </div>
             </div>
+
         </nav>
         <main>
             @yield('home-content')
